921f8939d07f67e08e1756316c6c69d63c1edc03
Joseph Jackson <kodomo.oishii@gmail.com>
2023-10-21 14:30:02 -0700

    fade in

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const COLORS = {
-  100: '',
-  101: '',
+  100: '#ffffff',
+  101: '#ffffff',
   201: '#ff0000',
   202: '#ff9900',
   203: '#ccff00',
@@ -16,10 +16,10 @@ const COLORS = {
   210: '#ff0099',
 };
 
-function Tile({num}) {
+function Tile({num, cur}) {
   return (
-    <div className='leading-9' style={{color: COLORS[num]}}>
-      {num > 200 ? num-201 : '.'}
+    <div className='leading-9 duration-1000 ease-in-out' style={{color: num < cur && COLORS[num]}}>
+      {num < 200 ? '.' : num-201}
     </div>
   )
 }
@@ -27,8 +27,10 @@ function Tile({num}) {
 function App() {
   const [day, setDay] = useState()
   const [solution, setSolution] = useState(null);
+  const [cur, setCur] = useState(201);
 
   useEffect(() => {
+    setTimeout(refreshSolution, 1000)
     refreshSolution();
   }, [day])
 
@@ -54,13 +56,16 @@ function App() {
           {solution.map((row, r) => (
             <div key={'row'+r} className='flex gap-5'>
               {row.map((num, c) => (
-                <Tile key={'col'+c} num={num}/>
+                <Tile key={'col'+c} num={num} cur={cur}/>
               ))}
             </div>
           ))}
         </div>
       )}
-      <div className='fixed top-0 left-0 right-0 bottom-0 bg-zinc-50 dark:bg-black backdrop-blur-lg hover:bg-transparent hover:backdrop-blur-none duration-500 ease-in-out'></div>
+      <button
+        className='fixed top-0 left-0 right-0 bottom-0 bg-transparent duration-500 ease-in-out'
+        onClick={() => setCur((cur-201+1)%11+201)}
+      ></button>
     </div>
   )
 }
