277203ec1bdc8e17e6c666c430ed49004b7dcd36
Joseph Jackson <kodomo.oishii@gmail.com>
2023-10-20 18:33:01 -0700

    Fixed colors and added spoiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const COLORS = {
-  100: '#000000',
-  101: '#000000',
+  100: '',
+  101: '',
   201: '#ff0000',
-  202: '#00ff00',
-  203: '#0000ff',
-  204: '#00ffff',
-  205: '#ff00ff',
-  206: '#ffff00',
-  207: '#ffffff',
-  208: '#ff0000',
-  209: '#ff0000',
-  210: '#ff00ff',
+  202: '#ff9900',
+  203: '#ccff00',
+  204: '#33ff00',
+  205: '#00ff66',
+  206: '#00ffff',
+  207: '#0066ff',
+  208: '#3300ff',
+  209: '#cc00ff',
+  210: '#ff0099',
 };
 
 function Tile({num}) {
   return (
-    <div className='' style={{color: COLORS[num]}}>
-      {num > 200 ? num-201 : 0}
+    <div className='leading-9' style={{color: COLORS[num]}}>
+      {num > 200 ? num-201 : '.'}
     </div>
   )
 }
@@ -47,15 +45,14 @@ function App() {
     let resp = await fetch('https://io.kamiak.org/apad/');
     let obj = await resp.json();
     setSolution(obj);
-    console.log(obj+'')
   }
 
   return (
-    <div className='font-mono bg-zinc-50 dark:bg-black w-full min-h-[100svh] px-0 sm:px-8 py-1 sm:py-8 flex flex-col justify-center items-center bg-gradient-to-br selection:bg-zinc-300 selection:text-zinc-800 dark:selection:bg-zinc-700 dark:selection:text-zinc-50 select-none duration-300 ease-in-out'>
+    <div className='cursor-pointer relative text-3xl font-mono bg-zinc-50 dark:bg-black text-zinc-50 dark:text-black w-full min-h-[100svh] px-4 py-8 flex flex-col justify-center items-center bg-gradient-to-br select-none'>
       {solution && (
-        <div>
+        <div className=''>
           {solution.map((row, r) => (
-            <div key={'row'+r} className='flex gap-4'>
+            <div key={'row'+r} className='flex gap-5'>
               {row.map((num, c) => (
                 <Tile key={'col'+c} num={num}/>
               ))}
@@ -63,6 +60,7 @@ function App() {
           ))}
         </div>
       )}
+      <div className='fixed top-0 left-0 right-0 bottom-0 bg-zinc-50 dark:bg-black backdrop-blur-lg hover:bg-transparent hover:backdrop-blur-none duration-500 ease-in-out'></div>
     </div>
   )
 }
